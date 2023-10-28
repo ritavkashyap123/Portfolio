037087c6221d31cadfa930a450d4eecb6a10b78d
Ritav Kashyap <ritav20-237@aec.ac.in>
2023-10-29 00:19:04 +0530

    updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,41 +1,44 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
-import "./navbar.scss"
+import "./navbar.scss";
 
 const Navbar = () => {
-  
   return (
     <div className="Navbar">
       <div className="nav">
         <input type="checkbox" id="nav-check" />
         <div className="nav-header">
-          <Link to="/"><div className="nav-title">RitChan</div></Link>
+          <Link to="/">
+            <div className="nav-title">RitChan</div>
+          </Link>
         </div>
         <div className="nav-btn">
           <label htmlFor="nav-check">
+            {/* <span />
             <span />
-            <span />
-            <span />
+            <span /> */}
+            <FaBars />
           </label>
         </div>
         <div className="nav-links">
-          <Link to='/home' className="link" target='_block'>
+          <Link to="/home" className="link" target="_block">
             Home
           </Link>
-          <Link to='/project' className="link" target='_block'>
+          <Link to="/project" className="link" target="_block">
             Projects
           </Link>
-          <Link to='/Experience' className="link" target='_block'>
+          <Link to="/Experience" className="link" target="_block">
             Experience
           </Link>
-          <Link to='/skill' className="link" target='_block'>
+          <Link to="/skill" className="link" target="_block">
             Skills
           </Link>
-          <Link to='/achievement' className="link" target='_block'>
+          <Link to="/achievement" className="link" target="_block">
             Achievements
           </Link>
-          <Link to='/contact' className="link" target='_block'>
+          <Link to="/contact" className="link" target="_block">
             Contact
           </Link>
         </div>
--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -6,7 +6,7 @@ import "./preloader.scss";
 import { useNavigate } from "react-router-dom";
 
 const Preloader = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
     <div className="startPage">
@@ -47,5 +47,3 @@ const Preloader = () => {
 };
 
 export default Preloader;
-
-
